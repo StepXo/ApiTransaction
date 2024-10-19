d159196ad1c9cc180fce292cbf8b3185669a1ace
kariaso <kevinleonardoa2013@gmail.com>
2024-10-19 01:43:35 -0500

    Correccion coneccion base de datos

--- src/main/java/com/emazon/ApiTransaction/Infrastructure/Adapters/Feign/IUserFeign.java
+++ src/main/java/com/emazon/ApiTransaction/Infrastructure/Adapters/Feign/IUserFeign.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = InfraConstants.API_USER, url = "http://localhost:9090/admin",configuration = FeignConfiguration.class)
+@FeignClient(name = InfraConstants.API_USER, url = "${UserUrl}",configuration = FeignConfiguration.class)
 public interface IUserFeign {
 
     @GetMapping(value = InfraConstants.GET_USER,consumes = {MediaType.APPLICATION_JSON_VALUE},produces = {MediaType.APPLICATION_JSON_VALUE})
